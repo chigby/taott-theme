659da13a184ac16ddd390b0c76b8d724ac0b4659
Cameron Higby-Naquin <cameron.higbynaquin@gmail.com>
2012-05-27 18:33:28 -0400

    Initial theme files.

---
+++ content-page.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The template used for displaying page content in page.php
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<h1 class="entry-title"><?php the_title(); ?></h1>
+   <!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+		<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'twentyeleven' ) . '</span>', 'after' => '</div>' ) ); ?>
+	</div><!-- .entry-content -->
+		<?php edit_post_link( __( 'Edit', 'twentyeleven' ), '<span class="edit-link">', '</span>' ); ?>
+<!-- .entry-meta -->
+</article><!-- #post-<?php the_ID(); ?> -->
---
+++ footer.php
@@ -0,0 +1,7 @@
+<div style="clear:both;"></div>
+</div>
+<footer>
+&copy; 2012 Randall Miller and Matthew Montgomery
+</footer>
+
+</body></html>
